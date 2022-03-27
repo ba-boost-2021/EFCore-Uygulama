650dd3c590a7e52192f93be9719044c12131309e
Sergen Kahraman <sergen_kahraman_@hotmail.com>
2022-03-27 15:43:14 +0300

    Lock keyword'ü anlatıldı

---
+++ BilgeAdam.Lock/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace BilgeAdam.Lock
+{
+    internal class Program
+    {
+        public static void Main(string[] arg)
+        {
+            Console.WriteLine("Döngü için bir sayı giriniz:");
+            var input = int.Parse(Console.ReadLine());
+
+            var deneme = new Deneme(input);
+            var thread = new Thread(deneme.AddAllValues);
+            thread.Start();
+            Console.WriteLine("Silinecek verinin indeksini veriniz:");
+            var index = int.Parse(Console.ReadLine());
+            deneme.RemoveAt(index);
+        }
+    }
+
+    public class Deneme
+    {
+        private readonly object @lock = new object();
+        private readonly List<string> myList = new List<string>();
+        private int donguSayisi;
+
+        public Deneme(int donguSayisi)
+        {
+            this.donguSayisi = donguSayisi;
+        }
+
+        public void AddAllValues()
+        {
+            lock (@lock)
+            {
+                for (int i = 0; i < donguSayisi; i++)
+                {
+                    myList.Add($"{i}. dögü çalışıyor");
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("Döngü tamamlandı");
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            lock (@lock)
+            {
+                Console.WriteLine("silme işlemi başladı");
+                myList.RemoveAt(index);
+            }
+        }
+    }
+}
